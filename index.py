172e9c7087f5888c1fb072de87d3f4c4deed8d56
Nanashi <108890577+nk4913@users.noreply.github.com>
2023-03-04 19:57:44 +0000

    zfdx

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-from wsgi import app
+from app import app
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+if __name__ == "__main__":
+    app.run()
